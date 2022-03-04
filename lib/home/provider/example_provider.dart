6e689dc6963828bd3d735c99b6059f6e8facb9fd
katskoru <katskoru@gmail.com>
2022-03-04 19:18:29 +0100

    colors

--- lib/home/provider/example_provider.dart
+++ lib/home/provider/example_provider.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 
 class ExampleProvider extends ChangeNotifier {
   ExampleProvider() {
@@ -10,16 +11,24 @@ class ExampleProvider extends ChangeNotifier {
   _init() {
     _height = Random().nextInt(300).toDouble();
     _width = Random().nextInt(300).toDouble();
+    _color = Colors.amber;
   }
 
   double? _height;
   double? _width;
+  Color? _color;
 
   double? get height => _height;
   double? get width => _width;
+  Color? get color => _color;
 
   set height(double? newHeight) {
     _height = newHeight;
+    notifyListeners();
+  }
+
+  set color(Color? newColor) {
+    _color = newColor;
     notifyListeners();
   }
 
--- lib/home/screen/example.dart
+++ lib/home/screen/example.dart
@@ -9,12 +9,13 @@ class Example extends StatelessWidget {
   Widget build(BuildContext context) {
     var _width = Provider.of<ExampleProvider>(context).width;
     var _height = Provider.of<ExampleProvider>(context).height;
+    var _color = Provider.of<ExampleProvider>(context).color;
     print("hejo");
     return Column(
       children: [
         AnimatedContainer(
           duration: const Duration(milliseconds: 250),
-          color: Colors.green,
+          color: _color,
           width: _width,
           height: _height,
         )
